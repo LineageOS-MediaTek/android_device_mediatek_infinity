362f0622e8a1b14da528a4378c1cca4324e458be
Iscle <albertiscle9@gmail.com>
2018-07-17 19:07:02 +0000

    LD_AUO: BoardConfig.mk: added new linkers this should fix the non-booting problem

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,7 +68,11 @@ BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset 
 
 # Linker
 LINKER_FORCED_SHIM_LIBS := /system/vendor/bin/thermal|libshim_ifc.so
+LINKER_FORCED_SHIM_LIBS += /system/lib/libmedia.so|libshim_misc.so:/system/lib/libstagefright.so|libshim_misc.so:/system/lib/libandroid_runtime.so|libshim_misc.so
+LINKER_FORCED_SHIM_LIBS += /system/vendor/lib/libui_ext.so|libshim_ui.so:/system/vendor/lib/libcam_utils.so|libshim_ui.so
+LINKER_FORCED_SHIM_LIBS += /system/vendor/lib/libMtkOmxVenc.so|libshim_ui.so
 LINKER_FORCED_SHIM_LIBS += /system/vendor/lib/libgui_ext.so|libshim_gui.so
+LINKER_FORCED_SHIM_LIBS += /system/vendor/lib/mtk-ril.so|libshim_ifc.so
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216 
